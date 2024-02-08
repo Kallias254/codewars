d296fddc55b6da8814e85c641ee65f8e0ba47c5e
K254 <misedajoseph@gmail.com>
2024-02-08 13:00:41 +0300

    unique in order

--- 8kyu/welcome-to-the-city.js
+++ 8kyu/welcome-to-the-city.js
@@ -4,3 +4,6 @@
 //
 // ['John', 'Smith'], 'Phoenix', 'Arizona'
 // This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+function sayHello( name, city, state ) { 
+  return `Hello, ${name.join(" ")} Welcome to ${city}, ${state}!`
+}
